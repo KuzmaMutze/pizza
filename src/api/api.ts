8caac24a69e0bae89fff07264253570aa9de8957
Matvey <r3dw0r13@yandex.ru>
2021-05-20 16:44:40 +0400

    fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { PizzasType } from "../types/type";
 
 export const instance = axios.create({
-    baseURL: `http://localhost:3000/`,
+    baseURL: `https://pizza-herokua.herokuapp.com/`,
 })
 
 let data = (param: any) => {
